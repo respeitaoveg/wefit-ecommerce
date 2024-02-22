04507925b24a2b969c79ef976015cd83ae980af2
respeitaoveg <yuri.moc.rb@gmail.com>
2024-02-22 17:58:24 -0300

    feat: add correct implementation of normal Button to action

--- src/components/button/Button/index.tsx
+++ src/components/button/Button/index.tsx
@@ -1,17 +1,14 @@
 import { ReactNode } from "react"
-import { useNavigate } from "react-router-dom"
 import { ButtonContainer, Text } from "./styles";
 
 
 interface ButtonProps {
   children: ReactNode
-  path: string
+  onClick: () => void
 }
 
-export default function Button({ children, path }: ButtonProps) {
-  const navigate = useNavigate();
-
-  return <ButtonContainer onClick={() => navigate(path)}>
+export default function Button({ children, onClick }: ButtonProps) {
+  return <ButtonContainer onClick={() => onClick()}>
     <Text>
       {children}
     </Text>
--- src/pages/Finished/index.tsx
+++ src/pages/Finished/index.tsx
@@ -1,12 +1,15 @@
 import Button from "../../components/button/Button";
 import finishedImage from "../../assets/images/finished.png"
 import { CartContainer, Title } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Finished() {
+  const navigate = useNavigate();
+
   return <CartContainer>
     <Title>Compra realizada com sucesso!</Title>
     <img src={finishedImage} alt="Imagem em 2D fazendo um joinha e uma tela afrente ilustrando o step da compra" />
-    <Button path="/">VOLTAR</Button>
+    <Button onClick={() => navigate('/')}>VOLTAR</Button>
   </CartContainer>
 }
