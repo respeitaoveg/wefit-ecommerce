e2606466d45e676c84341420e849b9073ed1d062
respeitaoveg <yuri.moc.rb@gmail.com>
2024-02-22 15:49:05 -0300

    feat: add description to images

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -10,7 +10,7 @@ export default function Cart() {
 
   return <CartContainer>
     <Title>Parece que não há nada por aqui :( - {value.getCart().length}</Title>
-    <img src={emptyImage} alt="" />
+    <img src={emptyImage} alt="Imagem em 2D de uma mulher segurando um ícone grande de recarregamento com uma folha em branco de fundo" />
     <Button path="/">VOLTAR</Button>
   </CartContainer>
 }
--- src/pages/Finished/index.tsx
+++ src/pages/Finished/index.tsx
@@ -6,7 +6,7 @@ import { CartContainer, Title } from "./styles";
 export default function Finished() {
   return <CartContainer>
     <Title>Compra realizada com sucesso!</Title>
-    <img src={finishedImage} alt="" />
+    <img src={finishedImage} alt="Imagem em 2D fazendo um joinha e uma tela afrente ilustrando o step da compra" />
     <Button path="/">VOLTAR</Button>
   </CartContainer>
 }
