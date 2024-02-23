60494cd662a4b612099d74fac235ccb37fd472c7
respeitaoveg <yuri.moc.rb@gmail.com>
2024-02-23 02:15:41 -0300

    feat: minimalism

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./layouts/Header"
 import DefaultLayout from "./layouts/DefaultLayout";
 import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Container, ContainerSpinner, Spinner } from "./assets/css/app.styles";
 
 
