39efc6593b99c52432f2cacb79754f10849bf55f
respeitaoveg <yuri.moc.rb@gmail.com>
2024-02-23 03:09:50 -0300

    feat: start req with axios to get products

--- src/components/ListProducts/index.tsx
+++ src/components/ListProducts/index.tsx
@@ -1,49 +1,21 @@
-import { product } from "../../entities/product.entity";
-import Product from "../Product";
+import { useState } from "react";
+import { ClientApi } from "../../services/ClientApi";
+import ProductComponent from "../Product";
 import { ContainerListProducts } from "./styles";
-
-const productMock: product[] = [
-  {
-    "id": 1,
-    "title": "Viúva Negra",
-    "price": 9.99,
-    "image": "https://wefit-react-web-test.s3.amazonaws.com/viuva-negra.png"
-  },
-  {
-    "id": 2,
-    "title": "Shang-chi",
-    "price": 30.99,
-    "image": "https://wefit-react-web-test.s3.amazonaws.com/shang-chi.png"
-  },
-  {
-    "id": 3,
-    "title": "Homem Aranha",
-    "price": 29.9,
-    "image": "https://wefit-react-web-test.s3.amazonaws.com/spider-man.png"
-  },
-  {
-    "id": 1,
-    "title": "Viúva Negra",
-    "price": 9.99,
-    "image": "https://wefit-react-web-test.s3.amazonaws.com/viuva-negra.png"
-  },
-  {
-    "id": 2,
-    "title": "Shang-chi",
-    "price": 30.99,
-    "image": "https://wefit-react-web-test.s3.amazonaws.com/shang-chi.png"
-  },
-  {
-    "id": 3,
-    "title": "Homem Aranha",
-    "price": 29.9,
-    "image": "https://wefit-react-web-test.s3.amazonaws.com/spider-man.png"
-  },
-]
+import { product } from "../../entities/product.entity";
 
 
 export default function ListProducts() {
+  const [products, setProducts] = useState<product[] | []>([])
+
+  async function getProducts() {
+    const client = new ClientApi()
+    const aux = await client.getListProducts()
+
+    setProducts(aux || [])
+  }
+
   return <ContainerListProducts>
-    {productMock.map((product, index) => (<Product product={product} key={index} />))}
+    {getProducts()).map((product, index) => (<ProductComponent product={product} key={index} />))}
   </ContainerListProducts>
 }
---
+++ src/services/ClientApi.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance } from "axios"
+import { product } from "../entities/product.entity"
+
+export class ClientApi {
+  private _http: AxiosInstance
+
+  constructor() {
+    this._http = axios.create({
+      baseURL: 'http://localhost:3000'
+    })
+  }
+
+  async getListProducts(): Promise<product[] | undefined> {
+    try {
+      const response = await this._http.get('/products')
+
+      return response.data
+    } catch (error) {
+      throw new Error(String(error))
+    }
+  }
+}
